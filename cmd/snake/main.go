8a480a99e9f84007bc920efe9bc00cb28295eb27
vinibispo <vini.bispo015@gmail.com>
2024-08-17 19:22:12 -0300

    Make the snake move

--- cmd/snake/main.go
+++ cmd/snake/main.go
@@ -8,6 +8,17 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+var lastUpdateTime float64
+
+func eventTriggered(interval float64) bool {
+	currentTime := rl.GetTime()
+	if currentTime-lastUpdateTime >= interval {
+		lastUpdateTime = currentTime
+		return true
+	}
+	return false
+}
+
 func main() {
 	var size int32 = constants.CellSize * constants.CellCount
 	rl.InitWindow(size, size, "Retro Snake")
@@ -18,6 +29,11 @@ func main() {
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
+		if eventTriggered(0.2) {
+			snake.Update()
+		}
+
+		snake.Move()
 		rl.ClearBackground(colors.Green)
 		food.Draw()
 		snake.Draw()
--- internals/models/snake.go
+++ internals/models/snake.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Snake struct {
-	Body []rl.Vector2
+	Body      []rl.Vector2
+	Direction rl.Vector2
 }
 
 func NewSnake() *Snake {
@@ -18,6 +19,7 @@ func NewSnake() *Snake {
 			rl.NewVector2(5, 9),
 			rl.NewVector2(4, 9),
 		},
+		Direction: rl.NewVector2(1, 0),
 	}
 	return s
 }
@@ -26,7 +28,32 @@ func (s *Snake) Draw() {
 	for _, part := range s.Body {
 		x := part.X
 		y := part.Y
-		rect := rl.NewRectangle(x*constants.CellSize, y*constants.CellSize, constants.CellSize, constants.CellSize)
-		rl.DrawRectangleRounded(rect, 0.5, 6, colors.DarkGreen)
+		segment := rl.NewRectangle(x*constants.CellSize, y*constants.CellSize, constants.CellSize, constants.CellSize)
+		rl.DrawRectangleRounded(segment, 0.5, 6, colors.DarkGreen)
+	}
+}
+
+func (s *Snake) Update() {
+	head := s.Body[0]
+	newHead := rl.NewVector2(head.X+s.Direction.X, head.Y+s.Direction.Y)
+	s.Body = append([]rl.Vector2{newHead}, s.Body...)
+	s.Body = s.Body[:len(s.Body)-1]
+}
+
+func (s *Snake) Move() {
+	if rl.IsKeyPressed(rl.KeyUp) && s.Direction.Y != 1 {
+		s.Direction = rl.NewVector2(0, -1)
+	}
+
+	if rl.IsKeyPressed(rl.KeyDown) && s.Direction.Y != -1 {
+		s.Direction = rl.NewVector2(0, 1)
+	}
+
+	if rl.IsKeyPressed(rl.KeyLeft) && s.Direction.X != 1 {
+		s.Direction = rl.NewVector2(-1, 0)
+	}
+
+	if rl.IsKeyPressed(rl.KeyRight) && s.Direction.X != -1 {
+		s.Direction = rl.NewVector2(1, 0)
 	}
 }
