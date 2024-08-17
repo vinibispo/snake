adf7bc0866a8c9731e0ee46131c68d4d2f75b68b
vinibispo <vini.bispo015@gmail.com>
2024-08-17 20:05:23 -0300

    Make the Snake eat the Food

--- internals/models/food.go
+++ internals/models/food.go
@@ -5,12 +5,21 @@ import (
 	constants "snake/internals"
 )
 
+func elementInSlice(element rl.Vector2, list []rl.Vector2) bool {
+	for _, b := range list {
+		if rl.Vector2Equals(element, b) {
+			return true
+		}
+	}
+	return false
+}
+
 type Food struct {
 	Position rl.Vector2
 	Texture  rl.Texture2D
 }
 
-func NewFood() *Food {
+func NewFood(snakeBody []rl.Vector2) *Food {
 	image := rl.LoadImage("assets/food.png")
 	texture := rl.LoadTextureFromImage(image)
 	rl.UnloadImage(image)
@@ -18,14 +27,22 @@ func NewFood() *Food {
 		Position: rl.NewVector2(5, 6),
 		Texture:  texture,
 	}
-	f.Position = f.Randomize()
+	f.Position = f.GenRandomPos(snakeBody)
 	return f
 }
 
-func (f *Food) Randomize() rl.Vector2 {
+func (f *Food) GenRandomCell() rl.Vector2 {
 	x := rl.GetRandomValue(0, constants.CellCount-1)
 	y := rl.GetRandomValue(0, constants.CellCount-1)
 	return rl.NewVector2(float32(x), float32(y))
+}
+
+func (f *Food) GenRandomPos(snakeBody []rl.Vector2) rl.Vector2 {
+	position := f.GenRandomCell()
+	for elementInSlice(position, snakeBody) {
+		position = f.GenRandomCell()
+	}
+	return position
 }
 
 func (f *Food) Draw() {
--- internals/models/game.go
+++ internals/models/game.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
 type Game struct {
 	Snake *Snake
 	Food  *Food
@@ -8,8 +12,8 @@ type Game struct {
 func NewGame() *Game {
 	g := &Game{
 		Snake: NewSnake(),
-		Food:  NewFood(),
 	}
+	g.Food = NewFood(g.Snake.Body)
 	return g
 }
 
@@ -20,8 +24,15 @@ func (g *Game) Draw() {
 
 func (g *Game) Update() {
 	g.Snake.Update()
+	g.CheckCollisionWithFood()
 }
 
 func (g *Game) Move() {
 	g.Snake.Move()
+}
+
+func (g *Game) CheckCollisionWithFood() {
+	if rl.Vector2Equals(g.Snake.Body[0], g.Food.Position) {
+		g.Food.Position = g.Food.GenRandomPos(g.Snake.Body)
+	}
 }
